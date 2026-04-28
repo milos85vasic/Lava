20676694b624fc902d9eddc264cbf261030182dc
Милош Васић <i@mvasic.ru>
2026-04-29 02:23:37 +0300

    Bump versions after release: api-go (--bump patch)

--- lava-api-go/internal/version/version.go
+++ lava-api-go/internal/version/version.go
@@ -9,8 +9,8 @@ package version
 
 const (
 	// Name is the service's semver. Tag prefix: Lava-API-Go-<Name>-<Code>.
-	Name = "2.0.0"
+	Name = "2.0.1"
 
 	// Code is the integer release counter. New tags MUST increment.
-	Code = 2000
+	Code = 2001
 )
